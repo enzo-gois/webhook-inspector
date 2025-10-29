d39d6693445acd11e0399fa6b97baa616e6ae26a
enzo gois <enzoab26@outlook.com>
2025-10-29 18:04:57 -0300

    feat: api routes and front-end integration

--- api/src/routes/delete-webhook.ts
+++ api/src/routes/delete-webhook.ts
@@ -1,40 +1,38 @@
-import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-import { createSelectSchema } from 'drizzle-zod'
-import z from 'zod'
-import { webhooks } from '@/db/schema'
-import { db } from '@/db'
-import { eq } from 'drizzle-orm'
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import z from "zod";
+import { webhooks } from "@/db/schema";
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
 
 export const deleteWebhooks: FastifyPluginAsyncZod = async (app) => {
-  app.delete(
-    '/api/webhooks/:id',
-    {
-      schema: {
-        summary: 'Delete a specific webhook by ID',
-        tags: ['Webhooks'],
-        querystring: z.object({
-          id: z.uuidv7(),
-        }),
-        response: {
-          200: z.void(),
-          404: z.object({ message: z.string() }),
-        },
-      },
-    },
-    async (request, reply) => {
-      const { id } = request.params
+	app.delete(
+		"/api/webhooks/:id",
+		{
+			schema: {
+				summary: "Delete a specific webhook by ID",
+				tags: ["Webhooks"],
+				params: z.object({
+					id: z.uuidv7(),
+				}),
+				response: {
+					204: z.void(),
+					404: z.object({ message: z.string() }),
+				},
+			},
+		},
+		async (request, reply) => {
+			const { id } = request.params;
 
-      const result = await db
-        .select()
-        .from(webhooks)
-        .where(eq(webhooks.id, id))
-        .limit(1)
+			const result = await db
+				.delete(webhooks)
+				.where(eq(webhooks.id, id))
+				.returning();
 
-      if (result.length === 0) {
-        return reply.status(404).send({ message: 'Webhook not found' })
-      }
+			if (result.length === 0) {
+				return reply.status(404).send({ message: "Webhook not found" });
+			}
 
-      return reply.send(result[0])
-    },
-  )
-}
+			return reply.status(204).send();
+		},
+	);
+};
